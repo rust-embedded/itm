2c4f52b46d614d1b1833f53dafb855bb576117cd
Jorge Aparicio <jorge@japaric.io>
2018-08-06 22:21:27 -0500

    CI: stop making binary releases

--- ci/script.sh
+++ ci/script.sh
@@ -1,19 +1,10 @@
-# This script takes care of testing your crate
-
-set -ex
+set -euxo pipefail
 
 main() {
-    cross check --target $TARGET
+    cargo check --target $TARGET
 
-    if [ ! -z $DISABLE_TESTS ]; then
-        return
-    fi
-
-    cross test --target $TARGET
-    cross test --target $TARGET --release
+    cargo test --target $TARGET
+    cargo test --target $TARGET --release
 }
 
-# we don't run the "test phase" when doing deploys
-if [ -z $TRAVIS_TAG ]; then
-    main
-fi
+main
